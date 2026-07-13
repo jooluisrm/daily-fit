2b89e5ce7993ed678b64df31ffca4214a6012d39
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2026-07-13 18:59:32 -0300

    erro vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
+"use client"
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      oi
-    </div>
-  );
+  const router = useRouter();
+
+  router.push("/login");
 }
